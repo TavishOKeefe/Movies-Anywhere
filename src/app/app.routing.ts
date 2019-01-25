5446cd8d93011413a167946851e619015581b2be
Padma Patil <padma@email.com>
2019-01-25 15:24:39 -0800

    add routs to show-selected-movie, search, and redeem component htmls.

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,12 +1,22 @@
 import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ShowSelectedMovieComponent } from './show-selected-movie/show-selected-movie.component';
+import { SearchComponent } from './search/search.component';
+import { RedeemComponent } from './redeem/redeem.component';
 
 const appRoutes: Routes = [
   {
-    path: '',
+    path: 'show-selected-movie',
     component: ShowSelectedMovieComponent
-  }
+  },
+  {
+    path: 'search',
+    component: SearchComponent
+  },
+  {
+    path: 'redeem',
+    component: RedeemComponent
+  },
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
