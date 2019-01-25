be43290cbb17b800e1932cefd00794fd8946f40a
Padma Patil <padma@email.com>
2019-01-25 11:41:23 -0800

    add redeem component. add basic html template for redeem component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MovieListComponent } from './movie-list/movie-list.component';
 import { ShowSelectedMovieComponent } from './show-selected-movie/show-selected-movie.component';
+import { RedeemComponent } from './redeem/redeem.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     MovieListComponent,
-    ShowSelectedMovieComponent
+    ShowSelectedMovieComponent,
+    RedeemComponent
   ],
   imports: [
     BrowserModule
