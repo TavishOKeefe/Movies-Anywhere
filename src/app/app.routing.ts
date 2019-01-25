7ca78e4a68279f2716f12059bc986575e3e300f4
Padma Patil <padma@email.com>
2019-01-25 15:36:05 -0800

    write paths in routing.ts to my-movies, movie-list, log-in, and get-started.

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,6 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 import { ShowSelectedMovieComponent } from './show-selected-movie/show-selected-movie.component';
 import { SearchComponent } from './search/search.component';
 import { RedeemComponent } from './redeem/redeem.component';
+import { MyMoviesComponent } from './my-movies/my-movies.component';
+import { MovieListComponent } from './movie-list/movie-list.component';
+import { LogInComponent } from './log-in/log-in.component';
+import { GetStartedComponent } from './get-started/get-started.component';
 
 const appRoutes: Routes = [
   {
@@ -16,6 +20,22 @@ const appRoutes: Routes = [
   {
     path: 'redeem',
     component: RedeemComponent
+  },
+  {
+    path: 'my-movies',
+    component: MyMoviesComponent
+  },
+  {
+    path: 'movie-list',
+    component: MovieListComponent
+  },
+  {
+    path: 'log-in',
+    component: LogInComponent
+  },
+  {
+    path: 'get-started',
+    component: GetStartedComponent
   },
 ];
 
